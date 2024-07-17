6f78ad9c31006f8644a8c47bd2627d47b8a1c507
mohit.kumar <mohit.kumar@transporeon.com>
2024-07-17 21:52:41 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import ejsLayouts from 'express-ejs-layouts';
+import path from 'path';
+
+const app = express();
+
+app.use(express.static('src/views'));
+app.use(express.static('public'))
+app.use(ejsLayouts);
+app.set('view engine', 'ejs');
+app.set('views', path.join(path.resolve(), 'src', 'views'));
+
+
+app.get('/', (req, res) => {
+  res.render('layout');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
